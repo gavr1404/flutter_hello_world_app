73ae927df1455baac823a6b9bff2535de8de45af
Gavr1404 <gavr140484@gmail.com>
2022-10-19 20:57:25 +0300

    finish_test

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("123Hello World"),
+          child: Text("Hello World"),
         ),
       ),
     ),
