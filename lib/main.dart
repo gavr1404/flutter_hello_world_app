ae79c295bca2bad2346f0a19a6d9c93cf5357ae8
Gavr1404 <gavr140484@gmail.com>
2022-10-19 20:07:51 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("Hello World1234"),
+          child: Text("Hello World"),
         ),
       ),
     ),
